81c9c858e70af41a8c4bdb30d951898ac8baac80
Honkawa Kojiro <kojiro-honkawa@geniee.jp>
2019-05-31 00:19:23 +0900

    dynamic response

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"os/exec"
     "github.com/gin-gonic/gin"
 )
+
+func str() string {
+	data, _ := exec.Command("ojichat", "-e10", "まっきー").Output()
+	return string(data)
+}
 
 func main() {
     router := gin.Default()
     router.LoadHTMLGlob("templates/*.html")
 
-	data := "Hello Go/Gin!!"
     router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{"data": data})
+        ctx.HTML(200, "index.html", gin.H{"data": str()})
     })
 
     router.Run()
